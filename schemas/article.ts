9471b9fd93b0b08509ab817578a6f4bcff82f4c0
Leda Wolf <leda@hey.com>
2023-11-29 16:11:15 +0100

    Correct content definition

--- schemas/article.ts
+++ schemas/article.ts
@@ -1,10 +1,10 @@
 export default {
-    name: 'article',
-    type: 'document',
-    title: 'Articles',
-    fields: [
-        {name: 'banner', type: 'image', title: 'Banner'},
-        {name: 'title', type: 'text', title: 'Title'},
-        {name: 'content', type: 'block', title: 'Content'},
-    ]
+  name: 'article',
+  type: 'document',
+  title: 'Articles',
+  fields: [
+    {name: 'banner', type: 'image', title: 'Banner'},
+    {name: 'title', type: 'text', title: 'Title'},
+    {title: 'Content', name: 'content', type: 'array', of: [{type: 'block'}]},
+  ],
 }
