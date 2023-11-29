c72a0b28cd2b2f19dabb68a9d3241b95fac4c38b
Leda Wolf <leda@hey.com>
2023-11-29 16:28:04 +0100

    Add validation

--- schemas/article.ts
+++ schemas/article.ts
@@ -1,11 +1,21 @@
+import {Rule} from 'sanity'
+
 export default {
   name: 'article',
   type: 'document',
   title: 'Articles',
   fields: [
     {name: 'banner', type: 'image', title: 'Banner'},
-    {name: 'title', type: 'string', title: 'Title'},
-    {name: 'slug', type: 'slug', title: 'URL slug'},
+    {name: 'title', type: 'string', title: 'Title', validation: (Rule: Rule) => Rule.required()},
+    {
+      name: 'slug',
+      type: 'slug',
+      title: 'URL slug',
+      options: {
+        source: 'title',
+        slugify: (input: string) => input.toLowerCase().replace(/\s+/g, '-').slice(0, 200),
+      },
+    },
     {title: 'Content', name: 'content', type: 'array', of: [{type: 'block'}]},
   ],
 }
