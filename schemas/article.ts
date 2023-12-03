8ea51c5e564132e0a848930c61f7269e7ea99288
Leda Wolf <leda@hey.com>
2023-12-03 15:19:35 +0100

    Add more block styles

--- schemas/article.ts
+++ schemas/article.ts
@@ -16,6 +16,29 @@ export default {
         slugify: (input: string) => input.toLowerCase().replace(/\s+/g, '-').slice(0, 200),
       },
     },
-    {title: 'Content', name: 'content', type: 'array', of: [{type: 'block'}, {type: 'image'}]},
+    {
+      title: 'Content',
+      name: 'content',
+      type: 'array',
+      of: [
+        {
+          type: 'block',
+          // Only allow these block styles
+          styles: [
+            {title: 'Normal', value: 'normal'},
+            {title: 'Heading 1', value: 'h1'},
+            {title: 'Heading 2', value: 'h2'},
+            {title: 'Heading 3', value: 'h3'},
+            {title: 'Heading 4', value: 'h4'},
+            {title: 'Heading 5', value: 'h5'},
+            {title: 'Heading 6', value: 'h6'},
+            {title: 'Quote', value: 'blockquote'},
+            {title: 'Cite', value: 'cite'},
+            {title: 'Subscript', value: 'sub' }
+          ],
+        },
+        {type: 'image'},
+      ],
+    },
   ],
 }
