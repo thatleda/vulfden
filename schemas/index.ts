1bd273004e56da5aa78617ab5c53470189b77ae3
Leda Wolf <leda@hey.com>
2024-05-26 16:43:56 +0200

    Remove clippings

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,7 +1,5 @@
 import article from "./article";
 import page from "./page";
-import clipping from "./clipping";
 import review from "./review";
-import book from "./book";
 
-export const schemaTypes = [review, article, page, clipping, book];
+export const schemaTypes = [review, article, page];
