d89c1e55942c9adb186f13ff9f2f7951db16a4a9
Leda Wolf <leda@hey.com>
2024-05-28 16:53:28 +0200

    Replace desk with structure and add vision tool

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,8 +1,9 @@
-import {defineConfig} from 'sanity'
-import {deskTool} from 'sanity/desk'
-import {schemaTypes} from './schemas'
-import {media} from 'sanity-plugin-media'
-import {netlifyTool} from 'sanity-plugin-netlify'
+import { visionTool } from "@sanity/vision"
+import { defineConfig } from 'sanity'
+import { media } from 'sanity-plugin-media'
+import { netlifyTool } from 'sanity-plugin-netlify'
+import { structureTool } from 'sanity/structure'
+import { schemaTypes } from './schemas'
 
 export default defineConfig({
   name: 'vulfden',
@@ -11,7 +12,8 @@ export default defineConfig({
   dataset: process.env.SANITY_STUDIO_DATASET ?? '',
 
   plugins: [
-    deskTool(),
+    visionTool(),
+    structureTool(),
     media(),
     netlifyTool(),
   ],
