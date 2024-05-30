f31e9248793acc80935bd567583480b8f4363ac1
Leda Wolf <leda@hey.com>
2024-05-30 12:10:09 +0200

    Add book to schema

---
+++ schemas/book.ts
@@ -0,0 +1,47 @@
+import { BookIcon } from "@sanity/icons";
+import { Rule } from "sanity";
+import block from "./block";
+import image from "./image";
+
+export default {
+    name: "book",
+    type: "document",
+    title: "Book",
+    icon: BookIcon,
+    fields: [
+        {
+            name: "cover",
+            type: "image",
+            title: "Cover"
+        }, 
+        {
+            name: "title",
+            type: "string",
+            title: "Title",
+            validation: (Rule: Rule) => Rule.required()
+        },
+        {
+            name: "author",
+            type: "string",
+            title: "Author",
+            validation: (Rule: Rule) => Rule.required()
+        },
+        {
+            name: "url",
+            type: "url",
+            title: "Available here",
+            validation: (Rule: Rule) => Rule.required()
+        },
+        {
+            name: "number",
+            type: "number",
+            title: "How many have I read this year?"
+        },
+        {
+            title: "Notes",
+            name: "notes",
+            type: "array",
+            of: [block, image]
+        }
+    ]
+}
--- schemas/index.ts
+++ schemas/index.ts
@@ -1,5 +1,6 @@
 import article from "./article";
+import book from "./book";
 import page from "./page";
 import review from "./review";
 
-export const schemaTypes = [review, article, page];
+export const schemaTypes = [review, article, page, book];
