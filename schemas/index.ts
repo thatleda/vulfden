52bf188c7f95c5b39b549cd6ec6f36cd399e4f1f
Leda Wolf <leda@hey.com>
2025-10-12 17:58:48 +0100

    remove book schema from index

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,6 +1,5 @@
 import article from './article'
-import book from './book'
 import page from './page'
 import review from './review'
 
-export const schemaTypes = [review, article, page, book]
+export const schemaTypes = [review, article, page]
