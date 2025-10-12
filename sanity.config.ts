5c21529b638a91ccd6386643c16675080a4c5ba9
Leda Wolf <leda@hey.com>
2025-10-12 19:07:26 +0100

    Remove nyanBannerPlugin from Sanity configuration

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,7 +2,6 @@ import { visionTool } from '@sanity/vision'
 import { theme } from 'https://themer.sanity.build/api/hues?preset=verdant'
 import { defineConfig } from 'sanity'
 import { media } from 'sanity-plugin-media'
-import { nyanBannerPlugin } from 'sanity-plugin-nyan-banner'
 import { structureTool } from 'sanity/structure'
 import { schemaTypes } from './schemas'
 
@@ -16,7 +15,6 @@ export default defineConfig({
     structureTool(),
     media(),
     visionTool(),
-    nyanBannerPlugin(),
   ],
 
   schema: {
