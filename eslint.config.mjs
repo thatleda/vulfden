ba00f083f13a7e70f12d2cbb8d675b3b737b73d6
Leda Wolf <leda.wolf@di-unternehmer.com>
2026-04-23 12:05:00 +0200

    chore: update dependencies in pnpm-workspace.yaml
    - Bump versions for eslint and related packages
    - Upgrade @antfu/eslint-config to 8.2.0
    - Upgrade @eslint-react/eslint-plugin to 4.2.3
    - Upgrade eslint to 10.2.1
    - Reorder shellEmulator and trustPolicy settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,6 @@
 import antfu from '@antfu/eslint-config'
 
 export default antfu({
-  react: true,
   rules: {
     'react-refresh/only-export-components': 'off',
     'node/prefer-global/process': 'off',
